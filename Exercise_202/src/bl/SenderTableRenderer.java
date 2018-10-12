cad1391feea7695fb8f0bd1eb3d01a41e9858efb
mroth19847 <rotmac16@htl-kaindorf.at>
2018-10-12 12:45:16 +0200

    created class cell renderer

---
+++ Exercise_202/src/bl/SenderTableRenderer.java
@@ -0,0 +1,15 @@
+
+package bl;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class SenderTableRenderer implements TableCellRenderer{
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        
+    }
+    
+}
